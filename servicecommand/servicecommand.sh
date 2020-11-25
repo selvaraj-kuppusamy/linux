4d94e4ac8af9b24cb0dbbc9fe9377346ffc33fb6
selvaraj-kuppusamy <velmanikvps@gmail.com>
2020-11-25 06:22:38 +0530

    service command

---
+++ servicecommand/servicecommand.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "service commnad in linux"
+echo "Starting an apache2 server on ubuntu"
+service apache2 start
+
+echo "Restaring an apache2 server on ubuntu"
+service apache2 restart
+
+echo "Stopping a apache2 server on ubuntu"
+service apache2 stop
+
